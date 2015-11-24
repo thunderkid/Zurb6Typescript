3707fd6e1c0eba1c6d0ba07475d69aa40c6e33a9
Adrian Cooper <adrian@smalliq.com>
2015-11-24 15:20:32 -0800

    Initial attempt at a minimal typescript + foundation6. Not working - jQuery not recognized when opening in browser.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var tsify = require('tsify');
+var source = require('vinyl-source-stream');
+
+
+var compilationCount = 1;
+function logCompilation(projName) {
+    console.log(' doing ts/js compilation # '+(compilationCount++)+ ' for ' + projName + ':');
+}
+
+
+function logError(str) {
+    //gutil.log(gutil.colors.bgYellow(str));
+    console.log(' error '+str);
+}
+
+
+function compiler(mainDir, mainFile, destDir, destFile) {
+    logCompilation('release '+destFile);
+    var bundler = browserify({basedir: mainDir, debug: true})
+        .add(mainFile)
+        .plugin(tsify, { noImplicitAny: false})
+        .on('error', function (error) { logError('error: ' + error.toString()); }) 
+
+    return bundler.bundle()
+            .pipe(source(destFile))
+            .pipe(gulp.dest(destDir));
+}
+
+
+gulp.task('compiletest', function() {
+    return compiler('./', 'ts/app.ts', 'jsBundles/', 'appbundle.js', true);
+});
+
---
+++ ts/app.ts
@@ -0,0 +1,8 @@
+declare var require;
+
+var foundation = require('foundation-sites');
+var jQuery = require('jquery');
+
+function pus() {
+	alert('this is pus');
+}
